94fa7b237c6e1b019fbcb122f2f6fa9803e99ce2
Kawsar Ahamed <kawsar019444@gmail.com>
2023-07-09 13:23:23 +0600

    basic all file done

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+const box = document.querySelector('.bulbbox');
+const offBulb = document.querySelector('.bulb .offbulb');
+const onBulb = document.querySelector('.bulb .onbulb');
+const button = document.querySelector(" button");
+
+onBulb.classList.add("dnone");
+
+button.addEventListener('click', () => {
+    onBulb.classList.toggle("dnone");
+    onBulb.classList.add("o");
+    offBulb.classList.toggle("dnone"); 
+
+
+    if (button.innerHTML === "ON") {
+        button.innerHTML = "OFF";
+        button.style. borderBottom= ("2px solid #979797");
+        button.style. borderRight= ("2px solid #979797");
+        button.style. borderLeft= ("None");
+        button.style. borderTop= ("None");
+       
+    } else {
+        button.innerHTML = "ON";
+        button.style. borderTop= ("2px solid #979797");
+        button.style. borderLeft= ("2px solid #979797");
+        button.style. borderRight= ("None");
+        button.style. borderBottom= ("None");
+        
+
+        
+    }
+
+})
+
+
+
+  
